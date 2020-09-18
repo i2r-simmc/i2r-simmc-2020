bcbe639256bc73500cd3448d21544ebfa9c971a6
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-18 15:33:58 +0800

    Update evaluate_all_models.sh

--- src/evaluate_all_models.sh
+++ src/evaluate_all_models.sh
@@ -1,1 +1,8 @@
-# To be written
+# Evaluate Bi-Encoder and Poly-Encoder for Fashion/Furniture
+
+python3 run.py --bart_model bart-base/ --output_dir data/simmc_fashion/ train_dir data/simmc_fashion/ --use_pretrain --architecture bi --eval 
+python3 run.py --bart_model bart-base/ --output_dir data/simmc_fashion/ train_dir data/simmc_fashion/ --use_pretrain --architecture poly --poly-m 16 --eval
+
+
+python3 run.py --bart_model bart-base/ --output_dir data/simmc_furniture/ train_dir data/simmc_furniture --use_pretrain --architecture bi --eval
+python3 run.py --bart_model bart-base/ --output_dir data/simmc_furniture/ train_dir data/simmc_furniture --use_pretrain --architecture poly --poly-m 16 --eval
