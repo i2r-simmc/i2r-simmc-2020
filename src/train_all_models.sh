48d65300ee5002900455755d4c986dc7279d7bc3
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-22 01:30:31 +0800

    Update train_all_models.sh

--- src/train_all_models.sh
+++ src/train_all_models.sh
@@ -1,8 +1,8 @@
 # Train Bi-Encoder and Poly-Encoder for Fashion/Furniture
 
-python3 run.py --bart_model bart-base/ --output_dir data/simmc_fashion/ --train_dir data/simmc_fashion/ --domain fashion --use_pretrain --architecture bi 
+#python3 run.py --bart_model bart-base/ --output_dir data/simmc_fashion/ --train_dir data/simmc_fashion/ --domain fashion --use_pretrain --architecture bi 
 python3 run.py --bart_model bart-base/ --output_dir data/simmc_fashion/ --train_dir data/simmc_fashion/ --domain fashion --use_pretrain --architecture poly --poly_m 16
 
 
-python3 run.py --bart_model bart-base/ --output_dir data/simmc_furniture/ --train_dir data/simmc_furniture --domain furniture --use_pretrain --architecture bi 
+#python3 run.py --bart_model bart-base/ --output_dir data/simmc_furniture/ --train_dir data/simmc_furniture --domain furniture --use_pretrain --architecture bi 
 python3 run.py --bart_model bart-base/ --output_dir data/simmc_furniture/ --train_dir data/simmc_furniture --domain furniture --use_pretrain --architecture poly --poly_m 16
