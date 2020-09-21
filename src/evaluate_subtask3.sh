73ff7680a53af431d7c358af02eb562652095059
Huang Xin <hxssg1124@gmail.com>
2020-09-21 21:41:52 +0800

    fix

--- src/evaluate_subtask3.sh
+++ src/evaluate_subtask3.sh
@@ -5,6 +5,6 @@ OUTPUT_ROOT=../output/$DOMAIN
 
 # Evaluate for devtest dataset
 python -m mm_dst.gpt2_dst.scripts.evaluate \
-    --input_path_target="${ROOT}"/${DOMAIN}_devtest_dials.json \
+    --input_path_target="${ROOT}"/${DOMAIN}_devtest_dials_target.txt \
     --input_path_predicted="${OUTPUT_ROOT}"/output_subtask3.txt_${INDEX} \
     --output_path_report="${OUTPUT_ROOT}"/output_subtask3_report.json_${INDEX}
