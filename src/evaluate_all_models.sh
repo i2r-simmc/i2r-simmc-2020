8a814899e9815fd027819314505992a7dd307d86
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-22 11:35:05 +0800

    Update evaluate_all_models.sh

--- src/evaluate_all_models.sh
+++ src/evaluate_all_models.sh
@@ -2,9 +2,9 @@
 
 # Evaluate Bi-Encoder and Poly-Encoder for Fashion/Furniture
 
-python3 run.py --bart_model bart-base/ --output_dir data/simmc_fashion/ train_dir data/simmc_fashion/ --use_pretrain --architecture bi --eval 
-python3 run.py --bart_model bart-base/ --output_dir data/simmc_fashion/ train_dir data/simmc_fashion/ --use_pretrain --architecture poly --poly-m 16 --eval
+python3 run.py --bart_model bart-base/ --output_dir data/simmc_fashion/ --train_dir data/simmc_fashion/ --use_pretrain --architecture bi --eval 
+python3 run.py --bart_model bart-base/ --output_dir data/simmc_fashion/ --train_dir data/simmc_fashion/ --use_pretrain --architecture poly --poly_m 16 --eval
 
 
-python3 run.py --bart_model bart-base/ --output_dir data/simmc_furniture/ train_dir data/simmc_furniture --use_pretrain --architecture bi --eval
-python3 run.py --bart_model bart-base/ --output_dir data/simmc_furniture/ train_dir data/simmc_furniture --use_pretrain --architecture poly --poly-m 16 --eval
+python3 run.py --bart_model bart-base/ --output_dir data/simmc_furniture/ --train_dir data/simmc_furniture --use_pretrain --architecture bi --eval
+python3 run.py --bart_model bart-base/ --output_dir data/simmc_furniture/ --train_dir data/simmc_furniture --use_pretrain --architecture poly --poly_m 16 --eval
