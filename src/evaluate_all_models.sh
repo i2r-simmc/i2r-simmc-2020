cd84d85663403f8436508a859936e8fbd4fde031
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-23 19:07:09 +0800

    Update evaluate_all_models.sh

--- src/evaluate_all_models.sh
+++ src/evaluate_all_models.sh
@@ -11,6 +11,8 @@ OUTPUT_DIR="../output/${DOMAIN}/"
 #ARCHITECTURE="poly"
 POLY_M=16
 
+echo "Performing evaluation for ${DOMAIN} dataset"
+
 python3 run.py --bart_model bart-base/ --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture bi --eval 
 python3 run.py --bart_model bart-base/ --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture poly --poly_m ${POLY_M} --eval
 
