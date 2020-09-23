c11ddf7aa333090f20c83457cfa8f5cf59764c87
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-23 14:29:42 +0800

    Update train_all_models.sh

--- src/train_all_models.sh
+++ src/train_all_models.sh
@@ -14,4 +14,4 @@ TESTSET="devtest"
 
 # Run training for bi-encoder and poly-encoder
 python3 run.py --bart_model bart-base/ --output_dir ${TRAIN_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture bi 
-python3 run.py --bart_model bart-base/ --output_dir ${TRAIN_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN --use_pretrain --architecture poly --poly_m ${POLY_M}
+python3 run.py --bart_model bart-base/ --output_dir ${TRAIN_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture poly --poly_m ${POLY_M}
