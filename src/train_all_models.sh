90a8794b815ef691c8036386cfeff465e20d2154
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-23 18:45:46 +0800

    Update train_all_models.sh

--- src/train_all_models.sh
+++ src/train_all_models.sh
@@ -4,11 +4,12 @@
 DOMAIN="fashion"
 #DOMAIN="furniture"
 TRAIN_DIR="./data/simmc_${DOMAIN}/"
+OUTPUT_DIR="../output/${DOMAIN}/"
 
 #ARCHITECTURE="bi"
 #ARCHITECTURE="poly"
 POLY_M=16
 
 # Run training for bi-encoder and poly-encoder
-python3 run.py --bart_model bart-base/ --output_dir ${TRAIN_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture bi 
-python3 run.py --bart_model bart-base/ --output_dir ${TRAIN_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture poly --poly_m ${POLY_M}
+python3 run.py --bart_model bart-base/ --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture bi 
+python3 run.py --bart_model bart-base/ --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture poly --poly_m ${POLY_M}
