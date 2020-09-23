d62d9a89cf1547b205387a61add791a873b31763
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-23 12:34:48 +0800

    Update parse.py

--- src/parse.py
+++ src/parse.py
@@ -15,6 +15,7 @@ with open('data/simmc_{}/{}_{}_dials_retrieval_candidates.json'.format(args.doma
     
 dial_index = 0
 
+print("Preprocessing " + 'data/simmc_{}/{}_{}_dials.json'.format(args.domain, args.domain, args.mode))
 print("Writing to " + 'data/simmc_{}/{}.txt'.format(args.domain, args.mode))
 
 outfile = open('data/simmc_{}/{}.txt'.format(args.domain, args.mode), 'w')
