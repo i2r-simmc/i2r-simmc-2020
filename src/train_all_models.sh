e31944b41891c7f2cae8640fffb964e49d4f6479
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-23 11:57:07 +0800

    Update train_all_models.sh

--- src/train_all_models.sh
+++ src/train_all_models.sh
@@ -5,18 +5,13 @@ DOMAIN="fashion"
 #DOMAIN="furniture"
 TRAIN_DIR="./data/simmc_${DOMAIN}/"
 
+ARCHITECTURE="bi"
+#ARCHITECTURE="poly"
+POLY_M=16
+
 TESTSET="devtest"
 #TESTSET="test-std"
 
-
-if [ $DOMAIN == "fashion" ]
-then
-    python3 run.py --bart_model bart-base/ --output_dir ${TRAIN_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture bi 
-    #python3 run.py --bart_model bart-base/ --output_dir data/simmc_fashion/ --train_dir data/simmc_fashion/ --domain fashion --use_pretrain --architecture bi 
-    #python3 run.py --bart_model bart-base/ --output_dir data/simmc_fashion/ --train_dir data/simmc_fashion/ --domain fashion --use_pretrain --architecture poly --poly_m 16
-elif [ $DOMAIN == "furniture" ]
-then
-    python3 run.py --bart_model bart-base/ --output_dir data/simmc_furniture/ --train_dir data/simmc_furniture --domain furniture --use_pretrain --architecture bi 
-    python3 run.py --bart_model bart-base/ --output_dir data/simmc_furniture/ --train_dir data/simmc_furniture --domain furniture --use_pretrain --architecture poly --poly_m 16
- 
-fi
+# Run training for bi-encoder and poly-encoder
+python3 run.py --bart_model bart-base/ --output_dir ${TRAIN_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture bi 
+python3 run.py --bart_model bart-base/ --output_dir ${TRAIN_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture poly --poly_m ${POLY_M}
