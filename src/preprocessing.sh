a548366432526520424d5dd4754a36055a9ab62f
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-23 12:28:05 +0800

    Update preprocessing.sh

--- src/preprocessing.sh
+++ src/preprocessing.sh
@@ -1,18 +1,18 @@
 # Preprocess data for both Fashion and Furniture domains 
 
 # Fashion
-python3 data/simmc_fashion/parse.py --mode train
-python3 data/simmc_fashion/parse.py --mode dev
-python3 data/simmc_fashion/parse.py --mode devtest
+python3 parse.py --mode train --domain fashion
+python3 parse.py --mode dev --domain fashion
+python3 parse.py --mode devtest --domain fashion
 
 mv data/simmc_fashion/devtest.txt data/simmc_fashion/test.txt
 cp data/simmc_fashion/fashion_devtest_dials_retrieval_candidates.json data/simmc_fashion/candidates.json
 
 
 #Furniture
-python3 data/simmc_furniture/parse.py --mode train
-python3 data/simmc_furniture/parse.py --mode dev
-python3 data/simmc_furniture/parse.py --mode devtest
+python3 parse.py --mode train --domain furniture
+python3 parse.py --mode dev --domain furniture
+python3 parse.py --mode devtest --domain furniture
 
 mv data/simmc_furniture/devtest.txt data/simmc_furniture/test.txt
 cp data/simmc_furniture/furniture_devtest_dials_retrieval_candidates.json data/simmc_furniture/candidates.json
