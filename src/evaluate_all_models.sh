cfb315b1c1fe9f44763bade5b6c701ca385c6434
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-23 18:46:33 +0800

    Update evaluate_all_models.sh

--- src/evaluate_all_models.sh
+++ src/evaluate_all_models.sh
@@ -5,11 +5,12 @@
 DOMAIN="fashion"
 #DOMAIN="furniture"
 TRAIN_DIR="./data/simmc_${DOMAIN}/"
+OUTPUT_DIR="../output/${DOMAIN}/"
 
 #ARCHITECTURE="bi"
 #ARCHITECTURE="poly"
 POLY_M=16
 
-python3 run.py --bart_model bart-base/ --output_dir ${TRAIN_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture bi --eval 
-python3 run.py --bart_model bart-base/ --output_dir ${TRAIN_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture poly --poly_m ${POLY_M} --eval
+python3 run.py --bart_model bart-base/ --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture bi --eval 
+python3 run.py --bart_model bart-base/ --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture poly --poly_m ${POLY_M} --eval
 
