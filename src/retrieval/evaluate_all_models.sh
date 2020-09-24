3caf68ac6d017b599ef81e177c651706751213c6
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-24 23:39:58 +0800

    Update evaluate_all_models.sh

--- src/retrieval/evaluate_all_models.sh
+++ src/retrieval/evaluate_all_models.sh
@@ -2,8 +2,8 @@
 
 # Evaluate Bi-Encoder and Poly-Encoder for Fashion/Furniture
 
-DOMAIN="fashion"
-#DOMAIN="furniture"
+#DOMAIN="fashion"
+DOMAIN="furniture"
 TRAIN_DIR="../../data/simmc_${DOMAIN}/"
 OUTPUT_DIR="../../output/${DOMAIN}/"
 MODEL_DIR="../../model/${DOMAIN}/best_model_fusion/"
@@ -14,6 +14,6 @@ POLY_M=16
 
 echo "Performing evaluation for ${DOMAIN} dataset"
 
-#python3 run.py --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture bi --eval 
+python3 run.py --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture bi --eval 
 python3 run.py --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture poly --poly_m ${POLY_M} --eval
 
