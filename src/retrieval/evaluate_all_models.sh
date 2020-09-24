e2e33c0d6ad48ddcc3930617debe350c9a75cc27
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-24 17:19:20 +0800

    Update evaluate_all_models.sh

--- src/retrieval/evaluate_all_models.sh
+++ src/retrieval/evaluate_all_models.sh
@@ -6,7 +6,7 @@
 DOMAIN="furniture"
 TRAIN_DIR="../../data/simmc_${DOMAIN}/"
 OUTPUT_DIR="../../output/${DOMAIN}/"
-MODEL_DIR="../../model/${DOMAIN}"
+MODEL_DIR="../../model/${DOMAIN}/best_model_fusion"
 
 #ARCHITECTURE="bi"
 #ARCHITECTURE="poly"
@@ -14,6 +14,6 @@ POLY_M=16
 
 echo "Performing evaluation for ${DOMAIN} dataset"
 
-python3 run.py --bart_model ${MODEL_DIR}/bart-base/ --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture bi --eval 
-#python3 run.py --bart_model ${MODEL_DIR}/bart-base/ --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture poly --poly_m ${POLY_M} --eval
+python3 run.py --bart_model ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture bi --eval 
+#python3 run.py --bart_model ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture poly --poly_m ${POLY_M} --eval
 
