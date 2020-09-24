25e96f76e0e1d9377cf16f522461e4c521a26f19
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-25 04:43:56 +0800

    Update evaluate_all_models.sh

--- src/retrieval/evaluate_all_models.sh
+++ src/retrieval/evaluate_all_models.sh
@@ -9,7 +9,7 @@ TRAIN_DIR="../../data/simmc_${DOMAIN}/"
 # Directory to output results
 OUTPUT_DIR="../../output/${DOMAIN}/"
 
-TESTSET='devtest"
+TESTSET="devtest"
 #TESTSET='test-std'
 
 # Directory where model is stored
@@ -23,4 +23,3 @@ echo "Performing evaluation for ${DOMAIN} dataset"
 
 python3 run.py --domain ${DOMAIN} --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --testset ${TESTSET} --use_pretrain --architecture bi --eval 
 python3 run.py --domain ${DOMAIN} --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --testset ${TESTSET} --use_pretrain --architecture poly --poly_m ${POLY_M} --eval
-
