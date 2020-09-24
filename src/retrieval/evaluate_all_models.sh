e2300879f7baa5971a3cdff7fc5f8400d5793b23
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-25 04:40:40 +0800

    Update evaluate_all_models.sh

--- src/retrieval/evaluate_all_models.sh
+++ src/retrieval/evaluate_all_models.sh
@@ -21,6 +21,6 @@ POLY_M=16
 
 echo "Performing evaluation for ${DOMAIN} dataset"
 
-python3 run.py --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --testset ${TESTSET} --use_pretrain --architecture bi --eval 
-python3 run.py --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --testset ${TESTSET} --use_pretrain --architecture poly --poly_m ${POLY_M} --eval
+python3 run.py --domain ${DOMAIN} --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --testset ${TESTSET} --use_pretrain --architecture bi --eval 
+python3 run.py --domain ${DOMAIN} --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --testset ${TESTSET} --use_pretrain --architecture poly --poly_m ${POLY_M} --eval
 
