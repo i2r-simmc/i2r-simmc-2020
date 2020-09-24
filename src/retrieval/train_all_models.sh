e1f36b98e92a28355c2f17316ff28066849f6fcd
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-24 23:42:34 +0800

    Update train_all_models.sh

--- src/retrieval/train_all_models.sh
+++ src/retrieval/train_all_models.sh
@@ -1,8 +1,9 @@
 # Train Bi-Encoder and Poly-Encoder for Fashion/Furniture
 
 # Select domain as appropriate
-DOMAIN="fashion"
-#DOMAIN="furniture"
+#DOMAIN="fashion"
+DOMAIN="furniture"
+
 # Directory where data is stored
 TRAIN_DIR="../../data/simmc_${DOMAIN}/"
 # Directory to output results
@@ -17,5 +18,5 @@ POLY_M=16
 echo "Performing training for ${DOMAIN} dataset"
 
 # Run training for bi-encoder and poly-encoder
-#python3 run.py --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture bi 
+python3 run.py --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture bi 
 python3 run.py --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture poly --poly_m ${POLY_M}
