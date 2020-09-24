46425550b2434821c646e63619e384503dc90e02
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-24 16:45:35 +0800

    Update train_all_models.sh

--- src/retrieval/train_all_models.sh
+++ src/retrieval/train_all_models.sh
@@ -14,5 +14,5 @@ POLY_M=16
 echo "Performing training for ${DOMAIN} dataset"
 
 # Run training for bi-encoder and poly-encoder
-python3 run.py --bart_model ${MODEL_DIR}/bart-base/ --output_dir ${OUTPUT_DIR} --output_dir ${MODEL_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture bi 
-#python3 run.py --bart_model ${MODEL_DIR}/bart-base/ --output_dir ${OUTPUT_DIR} --output_dir ${MODEL_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture poly --poly_m ${POLY_M}
+python3 run.py --bart_model ../bart-base/ --output_dir ${OUTPUT_DIR} --output_dir ${MODEL_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture bi 
+#python3 run.py --bart_model ../bart-base/ --output_dir ${OUTPUT_DIR} --output_dir ${MODEL_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture poly --poly_m ${POLY_M}
