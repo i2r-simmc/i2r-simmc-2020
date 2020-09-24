0773987b3654222edcc9b242c1f7e36466012748
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-25 04:34:36 +0800

    Update evaluate_all_models.sh

--- src/retrieval/evaluate_all_models.sh
+++ src/retrieval/evaluate_all_models.sh
@@ -8,6 +8,10 @@ DOMAIN="fashion"
 TRAIN_DIR="../../data/simmc_${DOMAIN}/"
 # Directory to output results
 OUTPUT_DIR="../../output/${DOMAIN}/"
+
+TESTSET='devtest"
+#TESTSET='test-std'
+
 # Directory where model is stored
 MODEL_DIR="../../model/${DOMAIN}/best_model_fusion/"
 
@@ -17,6 +21,6 @@ POLY_M=16
 
 echo "Performing evaluation for ${DOMAIN} dataset"
 
-python3 run.py --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture bi --eval 
-python3 run.py --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --use_pretrain --architecture poly --poly_m ${POLY_M} --eval
+python3 run.py --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --testset ${TESTSET} --use_pretrain --architecture bi --eval 
+python3 run.py --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --testset ${TESTSET} --use_pretrain --architecture poly --poly_m ${POLY_M} --eval
 
