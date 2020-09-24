3d06f9c5d748e79741faccc4bda1ddb0437e7770
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-24 17:35:33 +0800

    Update preprocess_retrieval.sh

--- src/preprocess_retrieval.sh
+++ src/preprocess_retrieval.sh
@@ -10,15 +10,8 @@ python3 ./retrieval/parse.py --mode train --domain fashion --train_dir ${TRAIN_D
 python3 ./retrieval/parse.py --mode dev --domain fashion --train_dir ${TRAIN_DIR}
 python3 ./retrieval/parse.py --mode ${TESTSET} --domain fashion --train_dir ${TRAIN_DIR}
 
-if [ $TESTSET == "devtest" ]
-then
-    mv ${TRAIN_DIR}simmc_fashion/devtest.txt ${TRAIN_DIR}simmc_fashion/test.txt
-    cp ${TRAIN_DIR}simmc_fashion/fashion_devtest_dials_retrieval_candidates.json ${TRAIN_DIR}simmc_fashion/candidates.json
-elif [ $TESTSET == "test-std" ]
-then
-    mv ../data/simmc_fashion/test-std.txt ../data/simmc_fashion/test.txt
-    cp ../data/simmc_fashion/fashion_test-std_dials_retrieval_candidates.json data/simmc_fashion/candidates.json
-fi
+mv ${TRAIN_DIR}simmc_fashion/${TESTSET}.txt ${TRAIN_DIR}simmc_fashion/test.txt
+cp ${TRAIN_DIR}simmc_fashion/fashion_${TESTSET}_dials_retrieval_candidates.json ${TRAIN_DIR}simmc_fashion/candidates.json
 
 
 #Furniture
@@ -26,12 +19,6 @@ python3 ./retrieval/parse.py --mode train --domain furniture --train_dir ${TRAIN
 python3 ./retrieval/parse.py --mode dev --domain furniture --train_dir ${TRAIN_DIR}
 python3 ./retrieval/parse.py --mode ${TESTSET} --domain furniture --train_dir ${TRAIN_DIR}
 
-if [ $TESTSET == "devtest" ]
-then
-    mv ${TRAIN_DIR}simmc_furniture/devtest.txt ${TRAIN_DIR}simmc_furniture/test.txt
-    cp ${TRAIN_DIR}simmc_furniture/furniture_devtest_dials_retrieval_candidates.json ${TRAIN_DIR}simmc_furniture/candidates.json
-elif [ $TESTSET == "test-std" ]
-then
-    mv ${TRAIN_DIR}simmc_furniture/test-std.txt ${TRAIN_DIR}simmc_furniture/test.txt
-    cp ${TRAIN_DIR}simmc_furniture/furniture_test-std_dials_retrieval_candidates.json ${TRAIN_DIR}simmc_furniture/candidates.json
-fi
+mv ${TRAIN_DIR}simmc_furniture/${TESTSET}.txt ${TRAIN_DIR}simmc_furniture/test.txt
+cp ${TRAIN_DIR}simmc_furniture/furniture_${TESTSET}_dials_retrieval_candidates.json ${TRAIN_DIR}simmc_furniture/candidates.json
+
