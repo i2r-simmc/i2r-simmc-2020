c93398fe815f719c5dcccf5a31003e43afc1e3ba
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-24 17:19:57 +0800

    Update train_all_models.sh

--- src/retrieval/train_all_models.sh
+++ src/retrieval/train_all_models.sh
@@ -5,7 +5,7 @@ DOMAIN="fashion"
 #DOMAIN="furniture"
 TRAIN_DIR="../../data/simmc_${DOMAIN}/"
 OUTPUT_DIR="../../output/${DOMAIN}/"
-MODEL_DIR="../../model/${DOMAIN}/best_model_fusion"
+MODEL_DIR="../../model/${DOMAIN}/best_model_fusion/"
 
 #ARCHITECTURE="bi"
 #ARCHITECTURE="poly"
@@ -14,5 +14,5 @@ POLY_M=16
 echo "Performing training for ${DOMAIN} dataset"
 
 # Run training for bi-encoder and poly-encoder
-python3 run.py --bart_model ../bart-base/ --output_dir ${OUTPUT_DIR} --output_dir ${MODEL_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture bi 
-#python3 run.py --bart_model ../bart-base/ --output_dir ${OUTPUT_DIR} --output_dir ${MODEL_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture poly --poly_m ${POLY_M}
+python3 run.py --bart_model ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --output_dir ${MODEL_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture bi 
+#python3 run.py --bart_model ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --output_dir ${MODEL_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} --use_pretrain --architecture poly --poly_m ${POLY_M}
