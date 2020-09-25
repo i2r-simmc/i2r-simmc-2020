744ad6aaa7a8d20fc12e2c9c71d4e93efdb8d3d2
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-25 16:23:15 +0800

    Update preprocess_retrieval.sh

--- src/preprocess_retrieval.sh
+++ src/preprocess_retrieval.sh
@@ -10,8 +10,8 @@ python3 ./retrieval/parse.py --mode train --domain fashion --train_dir ${TRAIN_D
 python3 ./retrieval/parse.py --mode dev --domain fashion --train_dir ${TRAIN_DIR}
 python3 ./retrieval/parse.py --mode ${TESTSET} --domain fashion --train_dir ${TRAIN_DIR}
 
-mv ${TRAIN_DIR}simmc_fashion/${TESTSET}.txt ${TRAIN_DIR}simmc_fashion/test.txt
-cp ${TRAIN_DIR}simmc_fashion/fashion_${TESTSET}_dials_retrieval_candidates.json ${TRAIN_DIR}simmc_fashion/candidates.json
+#mv ${TRAIN_DIR}simmc_fashion/${TESTSET}.txt ${TRAIN_DIR}simmc_fashion/test.txt
+cp ${TRAIN_DIR}simmc_fashion/fashion_${TESTSET}_dials_retrieval_candidates.json ${TRAIN_DIR}simmc_fashion/${TESTSET}_candidates.json
 
 
 #Furniture
@@ -19,6 +19,6 @@ python3 ./retrieval/parse.py --mode train --domain furniture --train_dir ${TRAIN
 python3 ./retrieval/parse.py --mode dev --domain furniture --train_dir ${TRAIN_DIR}
 python3 ./retrieval/parse.py --mode ${TESTSET} --domain furniture --train_dir ${TRAIN_DIR}
 
-mv ${TRAIN_DIR}simmc_furniture/${TESTSET}.txt ${TRAIN_DIR}simmc_furniture/test.txt
-cp ${TRAIN_DIR}simmc_furniture/furniture_${TESTSET}_dials_retrieval_candidates.json ${TRAIN_DIR}simmc_furniture/candidates.json
+#mv ${TRAIN_DIR}simmc_furniture/${TESTSET}.txt ${TRAIN_DIR}simmc_furniture/test.txt
+cp ${TRAIN_DIR}simmc_furniture/furniture_${TESTSET}_dials_retrieval_candidates.json ${TRAIN_DIR}simmc_furniture/${TESTSET}_candidates.json
 
