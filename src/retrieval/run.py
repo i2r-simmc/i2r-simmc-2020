2e460af16ca673007842fbb0221882dbf54d4b23
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-27 00:59:25 +0800

    Update run.py

--- src/retrieval/run.py
+++ src/retrieval/run.py
@@ -275,6 +275,7 @@ if __name__ == '__main__':
     if args.eval:
         print('Loading parameters from', state_save_path)
         model.load_state_dict(torch.load(state_save_path))
+        print('Generating model scores...")
         test_result = eval_running_model(val_dataloader, test=True)
         if not args.generate:
             print (test_result)
