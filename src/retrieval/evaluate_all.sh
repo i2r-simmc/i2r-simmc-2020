286707d6eed372bb831fb4347e82484cf106a61b
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-26 14:42:01 +0800

    Update and rename evaluate_all_models.sh to evaluate_all.sh

--- src/retrieval/evaluate_all.sh
+++ src/retrieval/evaluate_all.sh
@@ -4,7 +4,7 @@
 
 DOMAIN="fashion"
 #DOMAIN="furniture"
-TESTSET="devtest"
+TEST_SPLIT_NAME="devtest"
 #TESTSET="test-std"
 
 BART_MODEL="bart-base"
@@ -26,7 +26,7 @@ fi
 # Directory where data is stored
 TRAIN_DIR="../../data/simmc_${DOMAIN}/"
 # Directory to output results
-OUTPUT_DIR="../../output/${DOMAIN}/${BART_MODEL}_${MODEL_LABEL}/${TESTSET}/"
+OUTPUT_DIR="../../output/${DOMAIN}/${BART_MODEL}_${MODEL_LABEL}/${TEST_SPLIT_NAME}/"
 # Directory where pretrained model is stored
 MODEL_DIR="../../model/${DOMAIN}/${BART_MODEL}/best_model/"
 # Directory to store trained model
@@ -40,9 +40,9 @@ fi
 
 GPU=1
 
-echo "Performing evaluation for ${DOMAIN} ${TESTSET} dataset with ${BART_MODEL} and ${MODEL_LABEL}"
+echo "Performing evaluation for ${DOMAIN} ${TEST_SPLIT_NAME} dataset with ${BART_MODEL} and ${MODEL_LABEL}"
 
 python3 run.py --bart_model ${BART_MODEL} --model_in ${MODEL_DIR} --model_out ${MODEL_OUT} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} \
- --testset ${TESTSET} --use_pretrain --architecture ${ARCHITECTURE} --poly_m ${POLY_M} --gpu ${GPU} --set_seed --eval 
+ --testset ${TEST_SPLIT_NAME} --use_pretrain --architecture ${ARCHITECTURE} --poly_m ${POLY_M} --gpu ${GPU} --set_seed --eval 
 
-#python3 run.py --domain ${DOMAIN} --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --testset ${TESTSET} --use_pretrain --architecture poly --poly_m ${POLY_M} --eval
+#python3 run.py --domain ${DOMAIN} --bart_model ${MODEL_DIR} --model_out ${MODEL_DIR} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --testset ${TEST_SPLIT_NAME} --use_pretrain --architecture poly --poly_m ${POLY_M} --eval
