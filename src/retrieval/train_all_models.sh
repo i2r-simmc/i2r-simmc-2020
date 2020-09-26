9a92f1d6e9840f7052d97d8e3eddfd6cab8f1ec7
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-27 01:54:42 +0800

    Update train_all_models.sh

--- src/retrieval/train_all_models.sh
+++ src/retrieval/train_all_models.sh
@@ -45,4 +45,4 @@ echo "Performing training for ${DOMAIN} dataset with ${BART_MODEL} and ${MODEL_L
 
 # Run training for bi-encoder and poly-encoder
 python3 run.py --bart_model ${BART_MODEL} --model_in ${MODEL_DIR} --model_out ${MODEL_OUT} --output_dir ${OUTPUT_DIR} --train_dir ${TRAIN_DIR} --domain ${DOMAIN} \
---testset ${TEST_SPLIT_NAME} --use_pretrain --architecture ${ARCHITECTURE} --poly_m ${POLY_M} --gpu ${GPU} --set_seed --seed 10
+--testset ${TEST_SPLIT_NAME} --use_pretrain --architecture ${ARCHITECTURE} --poly_m ${POLY_M} --gpu ${GPU} --set_seed 
