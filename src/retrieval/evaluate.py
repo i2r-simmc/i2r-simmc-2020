cd4d669ee99658ef1273ff905cbead115901c3f2
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-27 00:34:42 +0800

    Create evaluate.py

---
+++ src/retrieval/evaluate.py
@@ -0,0 +1,68 @@
+import json
+from collections import defaultdict
+import argparse
+import numpy as np
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--domain', type=str)
+parser.add_argument('--output_dir', required=True, type=str)
+parser.add_argument('--train_dir', default = '../data/', type=str)
+parser.add_argument('--testset', default = 'devtest', type = str)
+args = parser.parse_args()
+
+def evaluate_response_retrieval(gt_responses, model_scores, single_round_eval=False):
+    """Evaluates response retrieval using the raw data and model predictions.
+    Args:
+        gt_responses: Ground truth responses.
+        model_scores: Scores assigned by the model to the candidates.
+        single_round_eval: Evaluate only for the last turn.
+    If in single round evaluation model (mostly for hidden test-std split),
+    use hidden gt_index field. Else, 0th element is the ground truth for other
+    splits.
+    """
+    gt_index_pool = {
+        ii["dialogue_idx"]: ii for ii in gt_responses["retrieval_candidates"]
+    }
+    gt_ranks = []
+    for model_datum in model_scores:
+        dialog_id = model_datum["dialog_id"]
+        gt_datum = gt_index_pool[dialog_id]["retrieval_candidates"]
+        num_gt_rounds = len(gt_datum)
+        for round_id, round_datum in enumerate(model_datum["candidate_scores"]):
+            round_id = round_datum["turn_id"]
+            # Skip if single_round_eval and this is not the last round.
+            if single_round_eval and round_id != num_gt_rounds - 1:
+                continue
+
+            gt_index = gt_datum[round_id]["gt_index"]
+            current_turn = round_datum["turn_id"]
+            round_scores = round_datum["scores"]
+            gt_score = round_scores[gt_index]
+            gt_ranks.append(np.sum(np.array(round_scores) > gt_score) + 1)
+    gt_ranks = np.array(gt_ranks)
+    print("#Instances evaluated retrieval: {}".format(gt_ranks.size))
+
+    return {
+        "r1": np.mean(gt_ranks <= 1),
+        "r5": np.mean(gt_ranks <= 5),
+        "r10": np.mean(gt_ranks <= 10),
+        "mean": np.mean(gt_ranks),
+        "mrr": np.mean(1 / gt_ranks),
+    }
+
+
+print("Reading: {}".format(args["retrieval_json_path"]))
+
+with open(os.path.join(args.output_dir, 'dstc9-simmc-{}-{}-subtask-2-retrieval.json'.format(args.testset, args.domain)) as infile:
+    scores = json.load(infile)
+
+print("Reading: {}".format(args["model_score_path"]))
+
+with open(os.path.join(args.train_dir, 'simmc_{}/{}_{}_dials_retrieval_candidates.json'.format(args.domain, args.domain, args.mode))) as infile:
+    gt = json.load(infile)
+    
+retrieval_metrics = evaluate_response_retrieval(
+    gt_responses, model_scores, args["single_round_evaluation"]
+)
+print(retrieval_metrics)
