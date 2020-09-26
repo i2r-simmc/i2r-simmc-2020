d12c6471d60d03aa4b47176a0bd8ada14552cb24
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-26 21:55:14 +0800

    Update evaluate_all.sh

--- src/retrieval/evaluate_all.sh
+++ src/retrieval/evaluate_all.sh
@@ -30,7 +30,7 @@ TRAIN_DIR="../../data/simmc_${DOMAIN}/"
 # Directory to output results
 OUTPUT_DIR="../../output/${DOMAIN}/${MODEL_LABEL}/${TEST_SPLIT_NAME}/"
 # Directory where pretrained model is stored
-MODEL_DIR="../../model/${DOMAIN}/${MODEL_LABEL}/best_model/"
+MODEL_DIR="../../model/${DOMAIN}/${BART_MODEL}/best_model/"
 # Directory to store trained model
 if [ ${ARCHITECTURE} == "bi" ]
 then
